c86918331eb6f1792e4671000d83c44f0fd123b0
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-05-26 17:10:16 -0300

    feat: create server api

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('API initialized on port 3000');
+});
+
+app.get('/marcas', (req, res) => {
+    res.send('Hello world');
+});
