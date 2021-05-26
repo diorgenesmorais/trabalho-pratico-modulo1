6a2e9962b1331dcc583a5357566d405b6a82e075
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-05-26 18:36:32 -0300

    feat: read all json

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,17 @@
-function list(req, res) {
-    res.send('hello controller');
+import { promises as fs } from 'fs';
+
+function handlerError(res, error) {
+    console.log(error);
+    res.send({error: 'Ocorreu um erro'});
+}
+
+async function list(req, res) {
+    try {
+        const data = JSON.parse(await fs.readFile('car-list.json'));
+        res.send(data);
+    } catch (error) {
+        handlerError(res, error);
+    }
 }
 
 export {
